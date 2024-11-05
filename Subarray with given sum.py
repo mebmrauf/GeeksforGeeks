554430e8bd95c3d37cc925a939ba4a142afd6d4f
B M Rauf <115145418+mebmrauf@users.noreply.github.com>
2024-11-06 01:43:54 +0600

    Update Subarray with given sum.py

--- Subarray with given sum.py
+++ Subarray with given sum.py
@@ -1,17 +1,18 @@
 # https://www.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/1
 
 class Solution:
-    def subArraySum(self, arr, n, s):
-        left, right, total = 0, 0, 0
-        if s < min(arr):
-            return [-1]
-        while right < n:
-            total += arr[right]
-            while total > s:
-                total -= arr[left]
-                left += 1
-            if total == s:
-                return [left + 1, right + 1]
-            right += 1
-
+    def subArraySum(self, arr, target):
+        start = 0
+        currentSum = 0
+        
+        for end in range(len(arr)):
+            currentSum += arr[end]
+            
+            while currentSum > target and start <= end:
+                currentSum -= arr[start]
+                start += 1
+            
+            if currentSum == target:
+                return [start + 1, end + 1]
+        
         return [-1]
